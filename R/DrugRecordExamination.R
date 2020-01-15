e73af6703af619226869646a4687ef9db0d622d6
neal1202 <neal1202@ajou.ac.kr>
2020-01-15 09:19:19 +0900

    episode event table generation

--- R/DrugRecordExamination.R
+++ R/DrugRecordExamination.R
@@ -22,64 +22,69 @@ drugRecordExamination<-function(targetSubjectId,
   ## Dispense date of primary drug is index date
   ## Generate index date list in one person
   if (!nrow(primaryConceptRecords)==0){
-  
-  indexDateList <- primaryConceptRecords %>% filter(subjectId == targetSubjectId)
-  ## Checking all drug condition
-  ### The drug observation period is from the index date to the date as long as drug Observation Date.
-  ### Secondary drug should be in the range of drug observation period and eliminatory drug should not be in.
-  
-  
-  if(length(secondaryConceptIdList)!=0){
-    secondaryConceptRecordsOneSubject <-lapply(1:length(secondaryConceptIdList),function(i){secondaryConceptRecords[[i]] %>% filter (subjectId == targetSubjectId)})
     
-  }
-  
-  if(length(excludingConceptIdList)!=0){
-    excludingConceptRecordsOneSubject <-excludingConceptRecords %>% filter(subjectId == targetSubjectId)}
-  
-  drugConditionPassedDate <-c()
-  drugConditionPassedStartDate <- c()
-  drugConditionPassedEndDate <- c()
-  ###
-  
-  for(x in 1:nrow(indexDateList)){
+    indexDateList <- primaryConceptRecords %>% filter(subjectId == targetSubjectId)
+    ## Checking all drug condition
+    ### The drug observation period is from the index date to the date as long as drug Observation Date.
+    ### Secondary drug should be in the range of drug observation period and eliminatory drug should not be in.
     
-    inResult<-list()
-    endDateList <-list()
     
-    ##secondary concept
     if(length(secondaryConceptIdList)!=0){
-      for(i in 1:length(secondaryConceptIdList)){
+      secondaryConceptRecordsOneSubject <-lapply(1:length(secondaryConceptIdList),function(i){secondaryConceptRecords[[i]] %>% filter (subjectId == targetSubjectId)})
+      
+    }
+    
+    if(length(excludingConceptIdList)!=0){
+      excludingConceptRecordsOneSubject <-excludingConceptRecords %>% filter(subjectId == targetSubjectId)}
+    
+    drugConditionPassedDate <-c()
+    drugConditionPassedStartDate <- c()
+    drugConditionPassedEndDate <- c()
+    eventItem <- c()
+    ###
+
+    for(x in 1:nrow(indexDateList)){
+      
+      inResult<-list()
+      endDateList <-list()
+      
+      ##secondary concept
+      if(length(secondaryConceptIdList)!=0){
+        for(i in 1:length(secondaryConceptIdList)){
+          
+          secondaryConceptInPeriod <- dplyr::filter(secondaryConceptRecordsOneSubject[[i]],between(drugExposureStartDate,indexDateList[x,3],indexDateList[x,3]+drugInspectionDate))
+          
+          inResult<-append(inResult,list(secondaryConceptInPeriod[1,3]))
+          if (length(secondaryConceptInPeriod$drugExposureEndDate)!=0){
+            endDateList<-append(endDateList,list(unique(max(secondaryConceptInPeriod$drugExposureEndDate,na.rm =TRUE))))}
+        }
+      }
+      ##excluding concept
+      if(length(excludingConceptIdList)!=0){
+        excludingConceptInPeriod <- dplyr::filter(excludingConceptRecordsOneSubject,between(drugExposureStartDate,indexDateList[x,3],indexDateList[x,3]+drugInspectionDate))
         
-        secondaryConceptInPeriod <- dplyr::filter(secondaryConceptRecordsOneSubject[[i]],between(drugExposureStartDate,indexDateList[x,3],indexDateList[x,3]+drugInspectionDate))
+        outResult <- excludingConceptInPeriod[1,3]
         
-        inResult<-append(inResult,list(secondaryConceptInPeriod[1,3]))
-        if (length(secondaryConceptInPeriod$drugExposureEndDate)!=0){
-          endDateList<-append(endDateList,list(unique(max(secondaryConceptInPeriod$drugExposureEndDate,na.rm =TRUE))))}
+      }else{outResult<-NA}
+      ##
+      if(sum(is.na(inResult))==0 & sum(!is.na(outResult))==0){
+        drugConditionPassedStartDate[x]<- indexDateList[x,3]
+        targetCycleItem <- paste0(secondaryConceptInPeriod$drugExposureId,collapse = '_')
+        eventItem[x] <- targetCycleItem
+        if(!is.null(drugConditionPassedStartDate)){
+          endDate<-as.Date(max(do.call(rbind,endDateList),na.rm =TRUE),origin="1970-01-01")
+          drugConditionPassedEndDate[x]<- max(c(indexDateList[x,3],endDate),na.rm =TRUE)}
+        
       }
     }
-    ##excluding concept
-    if(length(excludingConceptIdList)!=0){
-      excludingConceptInPeriod <- dplyr::filter(excludingConceptRecordsOneSubject,between(drugExposureStartDate,indexDateList[x,3],indexDateList[x,3]+drugInspectionDate))
-      
-      outResult <- excludingConceptInPeriod[1,3]
-      
-    }else{outResult<-NA}
-    ##
-    if(sum(is.na(inResult))==0 & sum(!is.na(outResult))==0){
-      drugConditionPassedStartDate[x]<- indexDateList[x,3]
-      
-      if(!is.null(drugConditionPassedStartDate)){
-        endDate<-as.Date(max(do.call(rbind,endDateList),na.rm =TRUE),origin="1970-01-01")
-        drugConditionPassedEndDate[x]<- max(c(indexDateList[x,3],endDate),na.rm =TRUE)}
-      
-    }
-  }
-  ###
-  if(!is.null(drugConditionPassedStartDate)){
-    drugConditionPassedDate <- data.frame(drugConditionPassedStartDate,drugConditionPassedEndDate)
-    drugConditionPassedDate <- na.omit(drugConditionPassedDate)}else{drugConditionPassedDate <- data.frame()}
+    ###
+    if(!is.null(drugConditionPassedStartDate)){
+      drugConditionPassedDate <- data.frame(drugConditionPassedStartDate,drugConditionPassedEndDate,eventItem)
+      drugConditionPassedDate <- na.omit(drugConditionPassedDate)
+      drugConditionPassedDate <- drugConditionPassedDate[c(order(drugConditionPassedDate$drugConditionPassedStartDate)),]
+      drugConditionPassedDate <- unique(drugConditionPassedDate)
+      rownames(drugConditionPassedDate) <- NULL
+      }else{drugConditionPassedDate <- data.frame()}
   }else{drugConditionPassedDate<-data.frame()}
   return(drugConditionPassedDate)
 }
-
--- R/Main.R
+++ R/Main.R
@@ -59,9 +59,7 @@ extractTargetRegimen<-function(parameters,
                                maxCores
 )
 {   
- 
   if (class(parameters)!="regimenLists") stop ("regimenLists should be of type regimenLists")
-  
   regimenConceptId<-parameters$regimenConceptId
   regimenName<-parameters$regimenName
   includeDescendant<-parameters$includeDescendant
@@ -116,61 +114,84 @@ extractTargetRegimen<-function(parameters,
   # drug & cycle condition check (It will take some time)
   
   data<-lapply(unique(primaryConceptRecords$subjectId),function(x){try(gapDateExamination(x,
-                                                                    primaryConceptRecords=primaryConceptRecords,
-                                                                    secondaryConceptRecords=secondaryConceptRecords,
-                                                                    excludingConceptRecords=excludingConceptRecords,
-                                                                    drugInspectionDate=drugInspectionDate,
-                                                                    secondaryConceptIdList=secondaryConceptIdList,
-                                                                    excludingConceptIdList=excludingConceptIdList,
-                                                                    gapDateBetweenCycle=gapDateBetweenCycle,
-                                                                    gapDateBefore=gapDateBefore,
-                                                                    gapDateAfter=gapDateAfter))})
-  
-         
-#  cluster <- ParallelLogger::makeCluster(numberOfThreads = maxCores)
-#  data <- ParallelLogger::clusterApply(cluster, unique(primaryConceptRecords$subjectId), gapDateExamination,
-#                                     primaryConceptRecords=primaryConceptRecords,
-#                                      secondaryConceptRecords=secondaryConceptRecords,
-#                                     excludingConceptRecords=excludingConceptRecords,
-#                                     drugInspectionDate=drugInspectionDate,
-#                                     secondaryDrugConceptIdList=secondaryDrugConceptIdList,
-#                                     excludingDrugConceptIdList=excludingDrugConceptIdList,
-#                                     gapDateBetweenCycle=gapDateBetweenCycle,
-#                                     gapDateBefore=gapDateBefore,
-#                                     gapDateAfter=gapDateAfter)
-#  ParallelLogger::stopCluster(cluster)
+                                                                                          primaryConceptRecords=primaryConceptRecords,
+                                                                                          secondaryConceptRecords=secondaryConceptRecords,
+                                                                                          excludingConceptRecords=excludingConceptRecords,
+                                                                                          drugInspectionDate=drugInspectionDate,
+                                                                                          secondaryConceptIdList=secondaryConceptIdList,
+                                                                                          excludingConceptIdList=excludingConceptIdList,
+                                                                                          gapDateBetweenCycle=gapDateBetweenCycle,
+                                                                                          gapDateBefore=gapDateBefore,
+                                                                                          gapDateAfter=gapDateAfter,
+                                                                                          regimenConceptId=regimenConceptId))})
+
   ParallelLogger::logInfo("extraction finish")
   data <- na.omit(data.table::rbindlist(data))
-  data<-recordsInEpisodeTableForm(data,regimenConceptId)
   return(data)
 }
 #' @export
-recordsInEpisodeTableForm<- function(regimenRecords,regimenConceptId){
-
+recordsInEpisodeTableForm<- function(regimenRecords){
+  
   regimenRecords$CYCLE_START_DATE<-as.Date(regimenRecords$CYCLE_START_DATE,origin="1970-01-01")
   regimenRecords$CYCLE_END_DATE<-as.Date(regimenRecords$CYCLE_END_DATE,origin="1970-01-01")
   regimenRecords$episode_type_concept_id <-32545
   regimenRecords$episode_concept_id <-32532
   regimenRecords$episode_parent_id <-NA
   regimenRecords$episode_object_concept_id <-32525
-  regimenRecords$episode_id <-1
+  regimenRecords$episode_id <-seq(nrow(regimenRecords))
   regimenRecords$episode_source_value <-NA
-  regimenRecords$episode_source_concept_id <-regimenConceptId
+
   
   names(regimenRecords) <- c('person_id',
-                                'episode_start_datetime',
-                                'episode_number',
-                                'episode_end_datetime',
-                                'episode_type_concept_id',
-                                'episode_concept_id',
-                                'episode_parent_id',
-                                'episode_object_concept_id',
-                                'episode_id',
-                                'episode_source_value',
-                                'episode_source_concept_id')
+                             'episode_start_datetime',
+                             'episode_number',
+                             'episode_source_concept_id',
+                             'episode_end_datetime',
+                             'episode_item',
+                             'episode_type_concept_id',
+                             'episode_concept_id',
+                             'episode_parent_id',
+                             'episode_object_concept_id',
+                             'episode_id',
+                             'episode_source_value'
+                             )
   cycleList <- data.frame(regimenRecords)
-  regimenRecords <- cycleList[,c(9,1,6,2,4,7,3,8,5,10,11)]
-  return(regimenRecords)
+  regimenRecords <- cycleList[,c(11,1,8,2,5,9,3,10,7,12,4,6)]
+  episodeTable <- regimenRecords[,c(1:11)]
+  episodeItemTable <- regimenRecords[,c(1,12)]
+ 
+  
+  ## event table
+  EventTable <- data.table::data.table()
+  
+  n <- nrow(episodeItemTable) # total number of rows
+
+  for (i in 1:n){
+
+    name_index <- as.character(episodeItemTable[i, 1])
+    item_index <- as.character(episodeItemTable[i, 2])
+    
+    item_index_split_temp <- data.frame(strsplit(item_index, split = '_'))
+    episodeItemTable_temp <- data.frame(cbind(name_index, item_index_split_temp))
+    
+    names(episodeItemTable_temp) <- c("episode_id", "drug_exposure_id")
+    
+    EventTable <- rbind(EventTable, episodeItemTable_temp)
+  }
+  ParallelLogger::logInfo("Episode event table generated")
+EventTable$visit_ccurrence_id <- NA
+EventTable$condition_occurrence_id <- NA
+EventTable$procedure_occurrence_id <- NA
+EventTable$device_exposure_id <- NA
+EventTable$measurement_id <- NA
+EventTable$specimen_id <- NA
+EventTable$observation_id <- NA
+EventTable$note_id <- NA
+EventTable$cost_id <- NA
+
+episodeEventTable <- EventTable[,c(1,3,4,5,2,6,7,8,9,10,11)]
+
+  return(list(episodeTable,episodeEventTable))
 }
 #' @export
 generateEpisodeTable <- function(targetRegimenConceptIds,
@@ -181,17 +202,37 @@ generateEpisodeTable <- function(targetRegimenConceptIds,
                                  targetCohortId,
                                  maxCores){
   
-parameters <- parameterSetting(targetRegimenConceptIds=targetRegimenConceptIds)
-ParallelLogger::logInfo("parameter loaded")
+  parameters <- parameterSetting(targetRegimenConceptIds=targetRegimenConceptIds)
+  ParallelLogger::logInfo("parameter loaded")
+  
+  targetRegimenRecordsList <- lapply(1:length(parameters),function(i){
+    parameters<-parameters[[i]]
+    extractTargetRegimen(parameters =parameters,
+                         connectionDetails=connectionDetails,
+                         cohortTable=cohortTable,
+                         cdmDatabaseSchema=cdmDatabaseSchema,
+                         cohortDatabaseSchema=cohortDatabaseSchema,
+                         targetCohortId=targetCohortId,
+                         maxCores=maxCores)})
+  
+  targetRegimenRecords <- data.table::rbindlist(targetRegimenRecordsList)
+  episodeAndEventTable<-recordsInEpisodeTableForm(targetRegimenRecords)
+  return(episodeAndEventTable)}
 
-regimenEpisodeRecords <- lapply(1:length(parameters),function(i){extractTargetRegimen(parameters =parameters[[i]],
-                                     connectionDetails=connectionDetails,
-                                     cohortTable=cohortTable,
-                                     cdmDatabaseSchema=cdmDatabaseSchema,
-                                     cohortDatabaseSchema=cohortDatabaseSchema,
-                                     targetCohortId=targetCohortId,
-                                     maxCores=maxCores)})
-
-episodeTable <- data.table::rbindlist(regimenEpisodeRecords)
-
-return(episodeTable)}
+#' @export
+distributionTable <- function(episodeTable,episodeSourceConceptId){
+  
+  episode <- episodeTable %>% filter(episode_source_concept_id == episodeSourceConceptId)
+  maxCycleNumberPerPerson<-aggregate(episode$episode_number,by = list(episode$person_id), max)
+  colnames(maxCycleNumberPerPerson) <- c('person_id','Cycle_num')
+  
+  # Total count
+  totalCount<-length(unique(maxCycleNumberPerPerson$person_id))
+  
+  # Count the number of patients in the value of each cycle number
+  countCycle<-as.data.frame(maxCycleNumberPerPerson %>% group_by(Cycle_num) %>% summarise(n = n()))
+  countCycle$'%'<-round(prop.table(table(maxCycleNumberPerPerson$Cycle_num))*100, digits = 1)
+  sum<- sum(countCycle$n)
+  sumName<- paste0('N','(','total=',sum,')')
+  names(countCycle) <- c('Treatment cycle',sumName,'%')
+  return(countCycle)}
