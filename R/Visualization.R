8d14ec2b71419b961b7fb46aaaea952a5e553f24
Hokyun Jeon <neal1202@ajou.ac.kr>
2020-01-19 19:07:01 +0900

    add heatmap code

--- R/Visualization.R
+++ R/Visualization.R
@@ -64,41 +64,46 @@ episodeTableForVisualization <- function(connectionDetails,
   return(result)
 }
 #' @export 
-regimenHeatmap<-function(episodeTableFromDatabase,visualizationTargetRegimenId,heatmapInRatio = TRUE){
-  if(is.null(visualizationTargetRegimenId)){visualizationTargetRegimenId<-unique(episodeTableFromDatabase$episodeSourceConceptId)}else{}
+regimenHeatmap<-function(episodeTableFromDatabase,visualizationTargetRegimenId = NULL,heatmapInRatio = TRUE,maximumCycleNumber = NULL){
   
-  totalDistribution <-data.table::rbindlist(lapply(visualizationTargetRegimenId,function(episodeSourceConceptId){
-    targetRegimenDistributionTable<-distributionTable(episodeTable=episodeTableFromDatabase,
-                                                      targetEpisodeConceptId=episodeSourceConceptId)
-    names(targetRegimenDistributionTable) <- c('cycle','n','ratio','conceptName')
-    return(targetRegimenDistributionTable)}))
+  if(is.null(visualizationTargetRegimenId)){visualizationTargetRegimenId<-unique(episodeTableFromDatabase$episodeSourceConceptId)}
+  
+  totalDistribution <-data.table::rbindlist(
+    lapply(visualizationTargetRegimenId,function(episodeSourceConceptId){
+      targetRegimenDistributionTable<-distributionTable(episodeTable=episodeTableFromDatabase,
+                                                        targetEpisodeConceptId=episodeSourceConceptId)
+      names(targetRegimenDistributionTable) <- c('cycle','n','ratio','conceptName')
+      return(targetRegimenDistributionTable)})
+  )
+  if(!is.null(maximumCycleNumber)){
+    totalDistribution <- subset(totalDistribution,cycle <= maximumCycleNumber)
+  }
   
   if(heatmapInRatio){
     totalDistribution <- as_tibble(totalDistribution) %>% select(cycle, conceptName, ratio)
     class(totalDistribution$ratio) = "dbl"
-    plotdata <- tidyr::spread(totalDistribution, cycle, ratio)}else{totalDistribution <- as_tibble(totalDistribution) %>% select(cycle, conceptName, n)
-      class(totalDistribution$n) = "dbl"}
+    plotdata <- tidyr::spread(totalDistribution, cycle, ratio)
+  }else{totalDistribution <- as_tibble(totalDistribution) %>% select(cycle, conceptName, n)
+  class(totalDistribution$n) = "dbl"
+  plotdata <- tidyr::spread(totalDistribution, cycle, n)}
   
-     
-      
-  plotdata <- tidyr::spread(totalDistribution, cycle, n)
   # 
   plotdata <- as.data.frame(plotdata)
   plotdata[is.na(plotdata)] <- 0
   row.names(plotdata) <- plotdata$conceptName
   plotdata$conceptName <- NULL
   sort.order <- order(plotdata$"12")
-  colors <- brewer.pal(9, "Blues")
-  heatmap<-superheat(plotdata,
-                     scale = FALSE,
-                     left.label.text.size=3,
-                     left.label.size = 0.3,
-                     bottom.label.text.size=3,
-                     bottom.label.size = .05,
-                     heat.pal = colors,
-                     heat.pal.values = c(seq(0,0.3,length.out = 8),1),
-                     order.rows = sort.order,
-                     title = "Repeated cycle number in each regimen")
+  colors <- RColorBrewer::brewer.pal(9, "Blues")
+  heatmap<-superheat::superheat(plotdata,
+                                scale = FALSE,
+                                left.label.text.size=3,
+                                left.label.size = 0.3,
+                                bottom.label.text.size=3,
+                                bottom.label.size = .05,
+                                heat.pal = colors,
+                                heat.pal.values = c(seq(0,0.3,length.out = 8),1),
+                                order.rows = sort.order,
+                                title = "Repeated cycle number in each regimen")
   return(heatmap)
 }
 #'@export generateHeatmap
@@ -107,13 +112,12 @@ generateHeatmap <- function(connectionDetails,
                             oncologyDatabaseSchema,
                             episodeTable,
                             visualizationTargetRegimenId = NULL,
-                            heatmapInRatio = TRUE){
+                            heatmapInRatio = TRUE,
+                            maximumCycleNumber = NULL){
   episodeTableFromDatabase<- episodeTableForVisualization(connectionDetails,
                                                           vocaDatabaseSchema,
                                                           oncologyDatabaseSchema,
                                                           episodeTable)
   
-  regimenHeatmap(episodeTableFromDatabase,visualizationTargetRegimenId,heatmapInRatio)
+  regimenHeatmap(episodeTableFromDatabase,visualizationTargetRegimenId,heatmapInRatio,maximumCycleNumber)
 }
-write.csv(episodeTableFromDatabase,file = 'episode.csv', row.names = FALSE)
-getwd()
